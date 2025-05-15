5011ed5600fc1515967ad96be510d71bcdfd2154
Nancyweng5 <wengalex55@gmail.com>
2025-05-14 21:24:20 -0400

    Flask 4

--- app.py
+++ app.py
@@ -8,60 +8,35 @@ app = Flask(__name__)
 def index():
     try: 
         response = requests.get("https://genshin.dev/characters")
-        characters = response.json()
+        data = response.json()
+        top_anime = data.get('data', [])
     except:
-        characters = []
+        top_anime = []
 
-    return render_template("index.html", characters=characters)
-    
-    
-for pokemon in pokemon_list:
-        # Each Pokémon has a URL like "https://pokeapi.co/api/v2/pokemon/1/"
-        url = pokemon['url']
-        parts = url.strip("/").split("/")
-        id = parts[-1]  # The last part of the URL is the Pokémon's ID
-        
-        # We use the ID to build an image URL.
-        image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
-        
-        pokemons.append({
-            'name': pokemon['name'].capitalize(),
-            'id': id,
-            'image': image_url
-        })
-    
-    # We tell Flask to show the 'index.html' page and pass the list of Pokémon.
-    return render_template("index.html", pokemons=pokemons)
+    return render_template("index.html", anime_list=top_anime)
 
-# Route for the Pokémon details page
-@app.route("/pokemon/<int:id>")
-def pokemon_detail(id):
-    # We get detailed info for a specific Pokémon using its id.
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
-    data = response.json()
+def search():
+    anime_list = []
+    query = ""
+    if request.method == "POST":
+        query = request.form.get("query")
+        try:
+            response = requests.get(f"https://api.jikan.moe/v4/anime?q={query}&limit=10")
+            data = response.json()
+            anime_list = data.get("data", [])
+        except:
+            anime_list = []
+    return render_template("search.html", anime_list=anime_list, query=query)
     
-    # We extract extra details like types, height, weight, and stats.
-    types = [t['type']['name'] for t in data['types']]
-    height = data.get('height')
-    weight = data.get('weight')
-    name = data.get('name').capitalize()
-    image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
-    
-    # Get the Pokémon’s base stats (like hp, attack, defense, etc.)
-    stat_names = [stat['stat']['name'] for stat in data['stats']]
-    stat_values = [stat['base_stat'] for stat in data['stats']]
-    
-    # We tell Flask to show the 'pokemon.html' page with all these details.
-    return render_template("pokemon.html", pokemon={
-        'name': name,
-        'id': id,
-        'image': image_url,
-        'types': types,
-        'height': height,
-        'weight': weight,
-        'stat_names': stat_names,
-        'stat_values': stat_values
-    })
+@app.route("/anime/<int:anime_id>")
+def anime_detail(anime_id):
+    try:
+        response = requests.get(f"https://api.jikan.moe/v4/anime/{anime_id}")
+        data = response.json().get("data", {})
+    except:
+        data = {}
 
-if __name__ == '__main__':
+    return render_template("anime.html", anime=data)
+
+if __name__ == "__main__":
     app.run(debug=True)
