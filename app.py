feed6c19ef739d6f8a95d50d23b62f8c6f3d5d2c
Nancy Weng <wengalex55@gmail.com>
2025-05-15 09:25:08 -0400

    api 5

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-# Route for the home page
+
 @app.route("/")
 def index():
     try: 
-        response = requests.get("https://genshin.dev/characters")
+        response = requests.get("https://api.jikan.moe/v4/top/anime?limit=10")
         data = response.json()
         top_anime = data.get('data', [])
     except:
@@ -15,6 +15,7 @@ def index():
 
     return render_template("index.html", anime_list=top_anime)
 
+@app.route("/search", methods=["GET", "POST"])
 def search():
     anime_list = []
     query = ""
@@ -27,7 +28,7 @@ def search():
         except:
             anime_list = []
     return render_template("search.html", anime_list=anime_list, query=query)
-    
+
 @app.route("/anime/<int:anime_id>")
 def anime_detail(anime_id):
     try:
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\wenga\OneDrive\Documents\GitHub\flask-api-project-Nancyweng5\venv
+set VIRTUAL_ENV=C:\Users\nancy.weng24\Documents\GitHub\flask-api-project-Nancyweng5\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
